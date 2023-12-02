87736a137c5cca1f1ca63cc8031605f8263d15b6
PakkoFrank48 <pakko.pakk@gmail.com>
2023-12-01 21:06:42 -0600

    Array corregido

--- Simple/array.js
+++ Simple/array.js
@@ -1,9 +1,10 @@
-let mayor = (miArray, nAlto) => {
+let mayor = (miArray) => {
   let nAlto = Math.max(...miArray);
+  console.log(nAlto);
 };
-let miArray = [0, 1, 10, 3, 4, 5, 6, 7];
+let miArray = [0, 15, 1, 10, 3, 4, 5, 6, 7, 8];
 let resultado = mayor(miArray);
-//console.log(resultado);
+
 //funcion que le des un array de numeros y te diga cual es el mayor
 
 //con fallas
